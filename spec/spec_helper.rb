44b06929fa590b417080d7b511a90860ba0d2b19
Waldo <ben.walsham@meltingwaldo.com>
2011-11-18 05:40:09 +0000

    Spec helper now loads anything directly under the lib directory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
+ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+
 require "rspec"
+
+Dir[File.join(ROOT, "lib/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.mock_with :rspec
